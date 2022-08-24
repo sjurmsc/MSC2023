d5e5b6a1ca2543de6df0079ef2ff363eeb651c6f
= <=>
2022-08-24 14:58:58 +0200

    Preliminary messing about

--- Scripts/Architectures.py
+++ Scripts/Architectures.py
@@ -1,3 +1,32 @@
 """
 Contains the model architectures so that they may easily be called upon.
 """
+
+from tensorflow import keras
+from keras.layers import Input, Dense, Dropout, Conv1D, Conv2D
+
+
+
+def 2DTemporalBlock(o, shape, params):
+    """
+    Input, split into 1x1 convolution to maintain shape. The rest goes into
+    dilated convolution layer
+
+    Activation function: ReLU
+    dropout layers at the ends, to prevent overfitting
+    """
+    o = Input(shape=shape)(o)
+
+
+
+    # Output her nede
+    return o
+
+
+def 1DTemporalBlock():
+    """
+    """
+    pass
+
+
+def weight_share_cost():
--- Scripts/Log.py
+++ Scripts/Log.py
@@ -2,3 +2,20 @@
 Whenever a model is trained it should have the weights saved and some descriptive information readily available
 in a document, so that it is possible to know the settings used for the model.
 """
+from pathlib import Path
+import sys
+from datetime import datetime
+
+def log_it():
+    p = Path('./Models')
+    print(p.is_dir())
+    return p
+
+
+
+if __name__ == '__main__':
+    m = log_it()
+    n = datetime.now()
+    new = m.joinpath(str(n.strftime(r'%d-%m-%Y %H:%M:%S')))
+    print(new)
+    new.mkdir()
