4def24638593818376f621ed69a53055cdd12996
= <=>
2022-09-27 13:51:12 +0200

    Functions for image comparisons

--- Scripts/Log.py
+++ Scripts/Log.py
@@ -48,24 +48,57 @@ def replace_md_image(filepath):
 
 from PIL import PSDraw
 
-def compare_pred_to_gt_image(fp, im_pred, im_true, imagesize=(3508, 2480), font = 'carlito', fontsize=20):
+def compare_pred_to_gt_image(fp, im_pred, im_true, imagesize=(3508, 2480), font = 'carlito', fontsize=20, dpi=300):
     """
     Function creates a side by side image of the prediction versus the
     ground truth image
     """
-    d = PSDraw(fp) # fp?
+    d = PSDraw('TEMP/test') # fp?
     d.begin_document()
     d.setfont(font, fontsize)
+    raise_text = 20
 
     # Predicted image
-    d.image()
-    d.text()
+    l_box = pass
+    d.image(l_box, im_pred, dpi=dpi)
+    tl_loc = (l_box[0], l_box[1]-raise_text) # Text raised by 20 px
+    d.text(tl_loc, 'Predicted image')
 
     # True image
-    d.image()
-    d.text
+    r_box = pass
+    d.image(r_box, im_true, dpi=dpi)
+    tr_loc = (r_box[0], r_box[1]-raise_text)
+    d.text(tr_loc, 'Ground Truth')
     
     d.end_document()
+
+import numpy as np
+from numpy.linalg import norm
+
+def create_pred_image_from_1d(model, X, gt_data, ratio=1.33333):
+    # Decide based on stats which section is the best predicting
+    # Moving window statistics
+    samples = pass # Amount of rows
+    traces = int(ratio*samples)  #the breadth of the image is the aspect_ratio*height
+
+    pred = model.predict(X=X) # 
+
+    scr = []
+
+    # Decide what slice is best, by loss
+    for s_idx in range(len(gt_data)-traces):
+        s = slice(s_idx, s_idx+traces)
+        score = norm(pred[s]-gt_data[s], 2)
+        scr.append(score)
+
+    slce = np.index(np.min(scr))
+    s = slice(slce, slce+traces)
+
+    pred_matrix = pred[s] ; gt_matrix = gt_data[s]
+
+    return pred_matrix, gt_matrix
+
+
 
 
 #%% Only used for testing the code
