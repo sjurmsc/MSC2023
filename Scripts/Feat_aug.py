ea73b80d86258adbee26bbc2585400ce1c446966
= <=>
2022-11-09 12:02:09 +0100

    Implemented recursive find function

--- Scripts/Feat_aug.py
+++ Scripts/Feat_aug.py
@@ -5,6 +5,7 @@ import segyio
 import json
 from numpy import array
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 from os import listdir
 from pathlib import Path
 
@@ -53,29 +54,60 @@ def split_image_into_data_packets(traces, width_shape=7, dim=2, mode='cut_lower'
     return array(X)
 
 
-def sgy_to_keras_dataset(data_label_list, 
-                        test_size, 
-                        zrange: tuple = (None,), 
-                        validation = False, 
-                        normalize = False,
-                        random_state=1):
+def sgy_to_keras_dataset(X_data_label_list,
+                         y_data_label_list,
+                         test_size, 
+                         feature_dimension = 1,
+                         zrange: tuple = (None,), 
+                         validation = False, 
+                         normalize = 'MinMaxScaler',
+                         random_state=1):
     """
     random_state may be passed for recreating results
     """
     data_dict = load_data_dict()
-    dataset = []
+    dataset = [] # Features within must be in the shape X[,y]
+
+
+    # Something to evaluate that z is same for all in a feature
     
-    for key in data_label_list:
+    for key in X_data_label_list:
         data_dir = Path(data_dict[key])
         for fname in data_dir.glob('*.sgy'):
             trace, z = get_traces((data_dir / fname), zrange=zrange)
         # Add the data to the dataset, must be robust for AI, CPT or seismic data
+    
+    dataset = array(dataset)
+
+    
+    
+    # Normalization
+    if normalize == 'MinMaxScaler':
+        scaler = MinMaxScaler()
+        X_new = scaler.fit_transform(X, y)
+    
     train_dataset, test_dataset = train_test_split(dataset, test_size=test_size, random_state=random_state)  # dataset must be np.array
     
     if validation:
         test_dataset, val = train_test_split(test_dataset, test_size=test_size, random_state=random_state)
         return train_dataset, test_dataset, val
     return train_dataset, test_dataset
+
+
+def collect_sgy_data_in_dataset():
+    pass
+
+def match_files(X_folder_loc, y_folder_loc, file_extension='.sgy'):
+    """
+    Matches the features from two seperate traces by filename
+    Filenames corresponding to each other are returned as a list
+    of tuples in the form [(X_file, y_file)]
+    """
+    X_dir = Path(X_folder_loc); y_dir = Path(y_folder_loc)
+
+    X_prefixes = []
+    for fname in X_dir.glob('*'+file_extension):
+        [fname[fname.find('_', 3)]]
 
 
 def pair_well_and_seismic():
@@ -90,7 +122,7 @@ def pair_well_and_seismic():
 def load_data_dict():
     data_json = './Data/data.json'
     with open(data_json, 'r') as readfile:
-        data_dict = readfile.read()
+        data_dict = json.loads(readfile.read())
     return data_dict
 
 
@@ -113,6 +145,15 @@ def update_data_dict():
 def combine_seismic_traces():
     pass
 
+
+def find_nth(haystack, needle, n):
+    n = abs(n)
+    max_needle_amount = len(haystack.split(needle))-1; assert n < max_needle_amount
+    if n:
+        loc = find_nth(haystack[haystack.find(needle)+1:], needle, n-1)
+    else:
+        return len(haystack[haystack.find(needle):])
+    return len(haystack) - loc
 
 def format_input_output(dataset):
     regression_data, seismic_data = dataset
