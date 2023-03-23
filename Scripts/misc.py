cc621daab6e38b3fd28c1205f1a5cce964c785aa
Sjur_NGI <=>
2023-03-23 15:10:46 +0100

    Investigating Structural model segy

--- Scripts/misc.py
+++ Scripts/misc.py
@@ -1,18 +1,26 @@
+import matplotlib.pyplot as plt
 
 def find_all_relevant_soil_units():
     import segyio
     import numpy as np
 
-    filename = r'P:\2019\07\20190798\Deliverables\Digital_Deliverables\02_IGM\00-StructuralModel\StructuralModel.sgy'
+    filename = '../OneDrive - NGI/Documents/NTNU/MSC_DATA/StructuralModel.sgy'
     with segyio.open(filename, ignore_geometry=True) as segyfile:
-        segyfile.mmap()
         z = segyfile.samples
         traces = segyio.collect(segyfile.trace)
+        cdps = segyio.collect(segyfile.attributes(segyio.TraceField.CDP))
+        lines = segyio.collect(segyfile.attributes(segyio.TraceField.INLINE_3D))
     
+    print('Done reading the seismic')
+
     # Find all unique values in the seismic
     unique_values = np.unique(traces.T[np.where(z<100)])
     print('The unique soil units in the upper 100m are: ', unique_values)
     print(traces.shape)
+
+    print(np.unique(lines))
+    plt.plot(cdps)
+    plt.show()
 
 if __name__ == '__main__':
     find_all_relevant_soil_units()
