db0f3642200bef106a725472e8acbe5f0879ed09
Sjur_NGI <=>
2023-04-11 12:40:23 +0200

    Adding padding

--- Scripts/Architectures.py
+++ Scripts/Architectures.py
@@ -56,23 +56,26 @@ def CNN_pyramidal_decoder(latent_features, image_width):
 
     inp = keras.layers.Input(shape=(None, latent_features))
     x = keras.layers.Reshape((-1, 1, latent_features))(inp) # Reshape to get features in the third dimension
+    x = keras.layers.ZeroPadding2D(padding=((0, 0), (1, 1)))(x)
     x = keras.layers.Conv2DTranspose(32, (3, 3), activation='relu', padding='same')(x)
     x = keras.layers.Conv2DTranspose(32, (3, 3), activation='relu', padding='same')(x)
-    x = keras.layers.ZeroPadding2D(padding=((0, 0), (1, 1)))(x) # 1, 1 padding because kernel is 3x3
-    x = keras.layers.Conv2DTranspose(32, (3, 3), activation='relu')(x) # Increase horizontal dimension by 2
+    x = keras.layers.Conv2DTranspose(32, (3, 3), activation='relu', padding='same')(x) # Increase horizontal dimension by 2
 
+    x = keras.layers.ZeroPadding2D(padding=((0, 0), (1, 1)))(x)
     x = keras.layers.Conv2DTranspose(64, (3, 3), activation='relu', padding='same')(x)
     x = keras.layers.Conv2DTranspose(64, (3, 3), activation='relu', padding='same')(x)
-    x = keras.layers.Conv2DTranspose(64, (3, 3), activation='relu')(x) # Increase horizontal dimension by 2
+    x = keras.layers.Conv2DTranspose(64, (3, 3), activation='relu', padding='same')(x) # Increase horizontal dimension by 2
 
+    x = keras.layers.ZeroPadding2D(padding=((0, 0), (1, 1)))(x)
     x = keras.layers.Conv2DTranspose(32, (3, 3), activation='relu', padding='same')(x)
     x = keras.layers.Conv2DTranspose(32, (3, 3), activation='relu', padding='same')(x)
     x = keras.layers.Conv2DTranspose(32, (3, 3), activation='relu', strides=(1, 2), padding='same')(x) # Increase horizontal dimension by 2, and vertical by factor 2
 
+    x = keras.layers.ZeroPadding2D(padding=((0, 0), (2, 2)))(x)
     x = keras.layers.Conv2DTranspose(16, (5, 5), activation='relu', padding='same')(x)
     x = keras.layers.Conv2DTranspose(16, (5, 5), activation='relu', padding='same')(x)
     x = keras.layers.Conv2DTranspose(16, (5, 5), activation='relu', padding='same')(x)
-    x = keras.layers.Conv2DTranspose(16, (5, 5), activation='relu')(x) # Increase horizontal dimension by 4
+    x = keras.layers.Conv2DTranspose(16, (5, 5), activation='relu', padding='same')(x) # Increase horizontal dimension by 4
 
     x = keras.layers.Conv2DTranspose(1, (image_width, 1), activation='linear', padding='same')(x) # Increase horizontal dimension to image_width
     outp = keras.layers.Reshape((-1, image_width, -1, 1))(x) # Reshape to get features in the second dimension
