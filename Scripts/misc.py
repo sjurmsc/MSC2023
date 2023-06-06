89040a5eb0fb1c7c954a8c40ed2a5e951a82037d
Sjur_NGI <=>
2023-06-06 17:29:34 +0200

    added karpathy model

--- Scripts/misc.py
+++ Scripts/misc.py
@@ -206,7 +206,139 @@ def plot_tsne_of_straight_line(n=16):
     fig.savefig('./Assignment Figures/tsne_of_straight_line.png', dpi=350)
 
 
+def karpathy_lstm():
+    """This function creates a simple LSTM model based on the Karpathy blog post."""
+    import keras
+    model = keras.Sequential([
+        keras.layers.LSTM(512, input_shape=(None, 1), return_sequences=True),
+        keras.layers.Dropout(0.5),
+        keras.layers.LSTM(512, return_sequences=True),
+        keras.layers.Dropout(0.5),
+        keras.layers.LSTM(512, return_sequences=True),
+        keras.layers.Dense(1)
+    ])
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+def train_karpathy_lstm():
+    """This function trains the Karpathy LSTM model."""
+    import tensorflow as tf
+    import numpy as np
+    import os
+    import time
+
+    path_to_file = tf.keras.utils.get_file('shakespeare.txt', 'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
+
+    # Read, then decode for py2 compat.
+    text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+    # length of text is the number of characters in it
+    print ('Length of text: {} characters'.format(len(text)))
+
+    # The unique characters in the file
+    vocab = sorted(set(text))
+    print ('{} unique characters'.format(len(vocab)))
+
+    # Creating a mapping from unique characters to indices
+    char2idx = {u:i for i, u in enumerate(vocab)}
+
+    idx2char = np.array(vocab)
+
+    text_as_int = np.array([char2idx[c] for c in text])
+
+    # The maximum length sentence we want for a single input in characters
+    seq_length = 100
+    examples_per_epoch = len(text)//(seq_length+1)
+
+    # Create training examples / targets
+    char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+
+    sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+    def split_input_target(chunk):
+        input_text = chunk[:-1]
+        target_text = chunk[1:]
+        return input_text, target_text
+    
+    dataset = sequences.map(split_input_target)
+
+    # Batch size
+    BATCH_SIZE = 100
+    steps_per_epoch = examples_per_epoch//BATCH_SIZE
+
+    # Buffer size to shuffle the dataset
+    # (TF data is designed to work with possibly infinite sequences,
+    # so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+    # it maintains a buffer in which it shuffles elements).
+    BUFFER_SIZE = 10000
+
+    dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+    # Length of the vocabulary in chars
+    vocab_size = len(vocab)
+    
+
+
+    model = karpathy_lstm()
+
+    X, y = [], []
+    for i in range(0, len(text_as_int)-seq_length, seq_length):
+        X.append(text_as_int[i:i+seq_length])
+        y.append(text_as_int[i+1:i+seq_length+1])
+    X = np.array(X)
+    y = np.array(y)
+
+    History = model.fit(X, y, epochs=100, batch_size=30, verbose=1)
+
+    # Print prediction
+
+    # Number of characters to generate
+    num_generate = 1000
+
+    prompt = 'Anders:'
+
+    # Converting our start string to numbers (vectorizing)
+    prompt = [char2idx[c] for c in prompt]
+    prompt = np.array(prompt)
+    prompt = prompt.reshape(1, -1)
+
+    # Empty string to store our results
+    text_generated = []
+
+    # Low temperatures results in more predictable text.
+    # Higher temperatures results in more surprising text.
+    # Experiment to find the best setting.
+    temperature = 1.0
+    
+    # Here batch size == 1
+    model.reset_states()
+    for i in range(num_generate):
+        predictions = model.predict(prompt)
+        # remove the batch dimension
+        predictions = tf.squeeze(predictions, 0)
+
+        # using a categorical distribution to predict the character returned by the model
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1,0].numpy()
+
+        # We pass the predicted character as the next input to the model
+        # along with the previous hidden state
+        prompt = np.array([[predicted_id]])
+        text_generated.append(idx2char[predicted_id])
+    
+    print ('Anders: {}'.format(''.join(text_generated)))
+
+
+    return History
+
+
 if __name__ == '__main__':
+
+    train_karpathy_lstm()
+
+    raise SystemExit
+
     from Feat_aug import *
     from Architectures import *
     from Log import *
